b2e4bf7bd7f3e346a1609688bd52638f62440536
sharon <ali93456@gmail.com>
2024-10-12 14:12:20 +0300

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	logger.Info("Starting server", slog.Any("PORT", *addr))
+
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sharon-xa/snippetbox
+
+go 1.23.2
