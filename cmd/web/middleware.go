cd855f1df58da9a11d443479e0ffe5ca51a4d519
sharon <ali93456@gmail.com>
2024-10-21 18:37:05 +0300

    chapter 7

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 )
@@ -36,6 +37,19 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 			slog.Any("method", method),
 			slog.Any("uri", uri),
 		)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
